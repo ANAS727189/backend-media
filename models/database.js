f34b080ba9c803208054daa63b84fca97605dc18
ANAS727189 <anas23khan083@gmail.com>
2024-10-10 14:13:12 +0530

    fix(fix): fixed the database error

--- models/database.js
+++ models/database.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/video-streaming")
+// Connect to MongoDB using Mongoose
+mongoose.connect("mongodb+srv://Anas:anaskhan083@cluster0.hvtkj.mongodb.net/Cluster0?retryWrites=true&w=majority")
   .then(() => console.log("Database connected"))
   .catch(err => console.log("Database connection failed", err));
 
-
+// Define your schema
 const videoSchema = new mongoose.Schema({
     title: String,
     description: String,
     videoPath: String,
     thumbnailPath: String,
     uploadDate: { type: Date, default: Date.now },
-})
+});
 
-export const Video = mongoose.model("Video", videoSchema)
+// Create a model based on the schema
+export const Video = mongoose.model("Video", videoSchema);
